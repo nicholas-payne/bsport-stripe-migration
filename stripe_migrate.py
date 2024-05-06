a25c13b5b05157feef00ad14bcc5331cea4b9af8
nicholas-payne <60719671+nicholas-payne@users.noreply.github.com>
2024-05-06 12:05:59 -0400

    Update stripe_migrate.py
    coupon and customer creation sections added to the main script

--- stripe_migrate.py
+++ stripe_migrate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import stripe
+import api_keys
 
 def stripe_migrate():
     """
@@ -11,14 +12,77 @@ def stripe_migrate():
     Customer Coupon ID,Date subscription will start,Address Line 1,Post code,City,IBAN
 
     Here is the workflow:
-    1. Create customers list in Stripe with name, email, address, language
-    2. Create coupon list in Stripe
+    1. Create coupon list in Stripe
+    2. Create customers list in Stripe with name, email, address, language
     3. Modify customer to add coupon code #THIS ORDERING IS VERY IMPORTANT TO AVOID CREATING INCORRECT INVOICES
-    4. Modify customer to add subscription
+    4. Modify customer to add subscription    
+    """
+    stripe.api_key = api_keys.stripe_api_key
+    
+    ######################
+    ## COUPONS CREATION ##
+    ######################
+
+    # Creating a dictionary of coupon durations and display names in this form {1: "1 Month Free",...}
+    coupon_durations = list(range(1,13)) 
+    coupon_names = list()
+
+    for duration in coupon_durations:
+        if duration == 1:
+            coupon_names.append(f'{duration} Month Free')
+        else:
+            coupon_names.append(f'{duration} Months Free')
+
+    coupons = dict(zip(coupon_durations,coupon_names))
+    
+    # Creating coupons and pulling unique ID's created by Stripe
+    for coupon in coupons:
+        stripe.Coupon.create(
+            duration = "repeating",
+            duration_in_months = coupon,
+            percent_off = 100,
+            coupon_currency = 'EUR',
+            name = coupons[coupon]
+            )
+        
+    # Retrieve Stripe coupon IDs generated via Stripe
+    coupon_list = stripe.Coupon.list()
+
+    stripe_coupon_ids = {}
+    for coupon in coupon_list['data']:
+        stripe_coupon_ids[coupon['duration_in_months']] = coupon['id']
+
+    #######################
+    ## CUSTOMER CREATION ##
+    #######################
+
+    # Read and upload customer details including coupon IDs created in Stripe
+    df = pd.read_csv('customer_details.csv')
+    df['Coupon_ID'] = df['Coupon_months'].map(stripe_coupon_ids)
+
+    for _,customer in df.iterrows():
+        stripe.Customer.create(
+            name=customer['Name'],
+            email=customer['email'],
+            address={'country':customer['Billing contry'],
+                    'city':customer['City'],
+                    'line1':customer['Address Line 1'],
+                    #'line2':customer['Address Line 2],
+                    'postal_code':customer['Post code']},
+            preferred_locales=[customer['Language']],
+            coupon = customer['Coupon_ID']
+            )
+
+    # Retrieve customer IDs generated via Stripe    
+    customer_list = stripe.Customer.list(limit=2)
+
+    stripe_customer_ids = {}
+    for customer in customer_list['data']:
+        stripe_customer_ids[customer['name']] = customer['id']
 
     
-    """
-    df = pd.read_csv('customer_details.csv')
+
+
 
 
 
